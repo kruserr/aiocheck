10c06f3a03f7ffd4ec85f25b7e2cc30be4dadfec
kruserr <46799551+kruserr@users.noreply.github.com>
2020-06-25 18:07:45 +0000

    added more testing for cli

--- tests/unit/cli/test_cli.py
+++ tests/unit/cli/test_cli.py
@@ -8,6 +8,7 @@ def test_cli():
     db = aiocheck.Database()
     try:
         os.remove(db.get_log_file())
+        os.remove(db.get_persist_file())
     except FileNotFoundError:
         pass
 
@@ -22,7 +23,6 @@ def test_cli():
     aiocheck.cli(
         'localhost',
         timeout=10,
-        menu=False,
     ).run_forever()
     with pytest.raises(FileNotFoundError):
         with open(db.get_log_file(), 'r') as f:
@@ -35,7 +35,72 @@ def test_cli():
         menu=False,
     ).run_forever()
     with open(db.get_log_file(), 'r') as f:
-        data = f.read()
-    rows = data.split('\n')
-    assert rows[0] == db.get_csv_header().replace('\n', '')
+        rows = f.readlines()
+    assert rows[0] == db.get_csv_header()
     assert rows[1].split(',')[0] == addr1
+    
+    addr2 = '10.20.30.50'
+    aiocheck.cli(
+        f"localhost {addr2}",
+        timeout=10,
+        menu=False,
+    ).run_forever()
+    with open(db.get_log_file(), 'r') as f:
+        rows = f.readlines()
+    assert rows[0] == db.get_csv_header()
+    assert rows[1].split(',')[0] == addr1
+    addr2_in_rows = False
+    for row in rows:
+        if row.split(',')[0] == addr2:
+            addr2_in_rows = True
+    assert addr2_in_rows == True
+    
+    with open(db.get_log_file(), "r") as f:
+        rows = f.readlines()[1:]
+    with open(db.get_log_file(), 'w') as f:
+        f.write("\n".join(rows))
+    aiocheck.cli(
+        f"localhost {addr2}",
+        timeout=10,
+        menu=False,
+    ).run_forever()
+    with open(db.get_log_file(), 'r') as f:
+        rows = f.readlines()
+    assert rows[0] == db.get_csv_header()
+    assert rows[1].split(',')[0] == addr2
+    
+    aiocheck.cli(
+        f"-m status localhost {addr1} {addr2}",
+        timeout=10,
+        menu=False,
+    ).run_forever()
+    with open(db.get_log_file(), 'r') as f:
+        rows = f.readlines()
+    assert rows[0] == db.get_csv_header()
+
+    addr_in_rows = []
+    for row in rows:
+        if row.split(',')[0] == addr1:
+            addr_in_rows.append(addr1)
+        if row.split(',')[0] == addr2:
+            addr_in_rows.append(addr2)
+
+    assert len(addr_in_rows) == 2
+    assert addr1 in addr_in_rows
+    assert addr2 in addr_in_rows
+    
+    aiocheck.cli(
+        f"-m status localhost {addr1}",
+        timeout=10,
+        menu=False,
+    ).run_forever()
+    with open(db.get_log_file(), 'r') as f:
+        rows = f.readlines()
+    assert rows[0] == db.get_csv_header()
+    assert rows[1].split(',')[0] == addr1
+
+    try:
+        os.remove(db.get_log_file())
+        os.remove(db.get_persist_file())
+    except FileNotFoundError:
+        pass
